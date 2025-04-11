c08fd4b4d0f540c4dd65475e171ef8f6ec140d6a
David Lecomber <david+dslarm@lecomber.net>
2025-04-11 22:04:25 +0100

    Update build.sh: add debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo libs..
+ls $PREFIX/lib
+echo include .. 
+ls $PREFIX/include
+
 perl Makefile.PL EXPATLIBPATH=$PREFIX/lib EXPATINCPATH=$PREFIX/include INSTALLDIRS=site
 make
 make install
